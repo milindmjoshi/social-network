7a8363faee46f39abee0c7d418289f6faf8d6e69
Milman <milindmjoshi@live.com>
2024-04-25 21:21:37 -0400

    Add initial project folders and files

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,//TODO: Add getter to format date
+    },
+    userName: {
+      type: String,
+      required: true,
+      ref: 'user', // TODO: Will this work?
+    },
+    reactions: [Reaction]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `responses` that gets the amount of response per video
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+        return this.reactions.length;
+  });
+
+// Initialize our Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+
+function trimName(name){
+  return name.trim();
+}
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    usename: { 
+        type: String,
+        required: true,
+        unique: true,
+        set: trimName,
+    },
+    email: { 
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+    age: Number,
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `fullName` that gets and sets the user's full name
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return  friends.length;
+  });
+   
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
